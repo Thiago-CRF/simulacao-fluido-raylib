fc448ac018d1788e10b7392c81b971e4f4eff1c9
Thiago <thiacefilho@gmail.com>
2026-01-07 21:22:43 -0300

    Todas as regras funcionando correto, cores e renderização erradas

--- fluid.c
+++ fluid.c
@@ -8,7 +8,7 @@ mudando o frametime na simulação, fazendo que seja o dobro
 #include <stdio.h>
 #include <stdlib.h>
 
-#define FPS 100
+#define FPS 50
 #define WIDTH 900
 #define HEIGHT 600
 #define GRID_THICK 2
@@ -21,14 +21,25 @@ mudando o frametime na simulação, fazendo que seja o dobro
 #define MAX_COMPRESSION 0.25
 
 #define WATER_BLUE (Color){69,189,245,255}
+#define DEEP_BLUE (Color){5,58,117,255}
 
 typedef struct
 {
     int type; // líquido ou sólido
-    float fill_level; // vazio: 0, cheio: 1. valor mínimo para ter água: 0.005
+    float fill_level; // vazio: 0, cheio: 1. valor mínimo para ter água: 0.0005
     int x, y; // coordenadas x, y
-    int flow_direction; // 0: cima. 1: baixo. 2: direita. 3: esquerda
+    int flow_direction; // 0: cima. 1: baixo. 2: direita. 3: esquerda. 4: ambos os lados
 } Cell;
+
+Color get_interpolate_color(Color max, Color min, double percentage)
+{
+    unsigned char R_new = (min.r - max.r) * percentage + max.r;
+    unsigned char G_new = (min.g - max.g) * percentage + max.g;
+    unsigned char B_new = (min.b - max.b) * percentage + max.b;
+    unsigned char A_new = (min.a - max.a) * percentage + max.a;
+
+    return (Color){R_new, G_new, B_new, A_new};
+}
 
 // Desenha o grid de divisão das células.
 void draw_grid()
@@ -49,7 +60,7 @@ void draw_cell(Cell cell)
     int pixel_x = cell.x * CELL_SIZE;
     int pixel_y = cell.y * CELL_SIZE;
 
-    if(cell.type == WATER_TYPE && cell.fill_level < 0.005)
+    if(cell.type == WATER_TYPE && cell.fill_level < 0.0005)
     {
         DrawRectangle(pixel_x, pixel_y, CELL_SIZE, CELL_SIZE, BLACK);
         return;
@@ -59,31 +70,46 @@ void draw_cell(Cell cell)
         DrawRectangle(pixel_x, pixel_y, CELL_SIZE, CELL_SIZE, WHITE);
         return;
     }
-    else
-    {
-        if(cell.fill_level >= 1)
-            DrawRectangle(pixel_x, pixel_y, CELL_SIZE, CELL_SIZE, WATER_BLUE);
-
-        /* ainda preciso diferenciar quando pode desenhar ela pela metade, e quando não pode
-        Quero que desenhe ela pela metade quando não tiver nada em cima e quando não estiver caindo
+    /* ainda preciso diferenciar quando pode desenhar ela pela metade, e quando não pode
+    Quero que desenhe ela pela metade quando não tiver nada em cima e quando não estiver caindo
         
-        Desenhar ela cheia, mesmo se tiver metade, caso esteja caindo ou esteja com outra agua em cima*/
-        else //if()
-        {
-            int water_height = cell.fill_level * CELL_SIZE;
-            int empty_height = CELL_SIZE - water_height;
-            DrawRectangle(pixel_x, (pixel_y + empty_height), CELL_SIZE, water_height, WATER_BLUE);
-            
-        }
-    }
+    Desenhar ela cheia, mesmo se tiver metade, caso esteja caindo ou esteja com outra agua em cima*/
     
+    Color interpolated_color = get_interpolate_color(WATER_BLUE, DEEP_BLUE, cell.fill_level);
+
+    if(cell.fill_level >= 1)
+    {
+        DrawRectangle(pixel_x, pixel_y, CELL_SIZE, CELL_SIZE, interpolated_color);
+        return;
+    }
+    else if(cell.flow_direction == 1)
+    {
+        DrawRectangle(pixel_x, pixel_y, CELL_SIZE, CELL_SIZE, interpolated_color);
+    }
+    else
+    //if(não tem água acima da célula)
+    {
+        // desenha a água no nível que ela está
+        int water_height = cell.fill_level * CELL_SIZE;
+        int empty_height = CELL_SIZE - water_height;
+        DrawRectangle(pixel_x, (pixel_y + empty_height), CELL_SIZE, water_height, interpolated_color);
+            
+    }
 }
 
 void draw_environment(Cell environment[])
 {
     for(int i=0; i<ROWS*COLUMNS; i++)
     {
+        // corrige valores de fluido antes de desenhar
+        if(environment[i].fill_level > 0 && environment[i].fill_level < 0.0005)
+        {
+            environment[i].fill_level = 0;
+        }
+
         draw_cell(environment[i]);
+        // reseta as direções após desenhar
+        environment[i].flow_direction = -1;
     }
 }
 
@@ -127,6 +153,12 @@ void sim_rule_1(Cell environment[], Cell updt_environment[])
         for(int j=0; j<COLUMNS; j++)
         {
             source_cell = &environment[j + COLUMNS*i];
+            // regra de eliminação
+            if(source_cell->fill_level <= 0.0005)
+            {
+                source_cell->fill_level = 0;
+            }
+
             if(source_cell->type == SOLID_TYPE || i == ROWS-1)
             // pula caso a celula seja solido, ou esteja na ultima linha
                 continue;
@@ -139,12 +171,19 @@ void sim_rule_1(Cell environment[], Cell updt_environment[])
             updt_source_cell = &updt_environment[j + COLUMNS*i];
             updt_cell_below = &updt_environment[j + COLUMNS*(i+1)];
 
+            // se a soma é menor ou um, passa tudo pra baixo
             float soma = (source_cell->fill_level + cell_below->fill_level);
             if(soma <= 1)
             {
                 flow = source_cell->fill_level;
+                if(flow < 0.005)
+                        continue;
+
                 updt_source_cell->fill_level -= flow;
                 updt_cell_below->fill_level += flow;
+
+                updt_cell_below->flow_direction = 1;
+                //updt_source_cell->flow_direction = 1;
             }
             /*outra ideia é fazer um calculo diferente caso a soma seja menor que 2 * (Valor_maximo+compressao_máxima)
             sendo o valor: (valor_maximo*valor_maximo + compressao_maxima*soma) / (valor_máximo + comressao_maxima).
@@ -155,14 +194,25 @@ void sim_rule_1(Cell environment[], Cell updt_environment[])
             {   // valor máximo: 1. Por enquanto quero que o valor máximo seja sempre 1, por isso não criei
                 // uma variavel ou uma constante para mudar o valor máximo no código todo
                 flow = (1 + MAX_COMPRESSION * soma) / (1 + MAX_COMPRESSION);
+                if(flow < 0.005)
+                    continue;
                 updt_source_cell->fill_level -= flow;
                 updt_cell_below->fill_level += flow;
+
+                updt_cell_below->flow_direction = 1;
+                //updt_source_cell->flow_direction = 1;
             }
             else
             {
                 flow = (soma + MAX_COMPRESSION) / 2;
+                if(flow < 0.005)
+                    continue;
+
                 updt_source_cell->fill_level -= flow;
                 updt_cell_below->fill_level += flow;
+
+                updt_cell_below->flow_direction = 1;
+                //updt_source_cell->flow_direction = 1;
             }
         }
     }
@@ -175,11 +225,12 @@ void sim_rule_1(Cell environment[], Cell updt_environment[])
 
 void sim_rule_2(Cell environment[], Cell updt_environment[])
 {   // TODO: refinar regra 2, parece que estou perdendo volume de água
+    // OBS: talvez a regra 3, da comopressão, resolva isso
 // array temporario para armazenar as mudanças
-    for(int i=0; i<ROWS*COLUMNS; i++)
+    /*for(int i=0; i<ROWS*COLUMNS; i++)
     {
         updt_environment[i] = environment[i];
-    }
+    } */
 
     Cell *source_cell, *right_cell, *left_cell;
     Cell *updt_source_cell, *updt_right_cell, *updt_left_cell;
@@ -190,7 +241,7 @@ void sim_rule_2(Cell environment[], Cell updt_environment[])
         for(int j=0; j<COLUMNS; j++)
         {
             source_cell = &environment[j + COLUMNS*i];
-            if(source_cell->type == SOLID_TYPE)
+            if(source_cell->type == SOLID_TYPE || source_cell->fill_level <= 0.0005)
                 continue;
 
             /* Precisa verificar se o lado direito ou esquerdo está bloqueado, 
@@ -216,9 +267,15 @@ void sim_rule_2(Cell environment[], Cell updt_environment[])
 
                 // calcula o fluxo e transfere apenas para direita
                 flow_out = source_cell->fill_level / 3;
+                if(flow_out < 0.005)
+                    continue;
+
                 flow_r = flow_out;
                 updt_source_cell->fill_level -= flow_out;
                 updt_right_cell->fill_level += flow_r;
+
+                updt_source_cell->flow_direction = 2;
+                //updt_right_cell->flow_direction = -1;
             }
 
             // caso esteja na direita da tela, flui só para esquerda
@@ -231,9 +288,15 @@ void sim_rule_2(Cell environment[], Cell updt_environment[])
 
                 // calcula o fluxo e transfere apenas para esquerda
                 flow_out = source_cell->fill_level / 3;
+                if(flow_out < 0.005)
+                    continue;
+
                 flow_l = flow_out;
                 updt_source_cell->fill_level -= flow_out;
                 updt_left_cell->fill_level += flow_l;
+
+                updt_source_cell->flow_direction = 3;
+                //updt_left_cell->flow_direction = -1;
             }
             else 
             {
@@ -250,30 +313,92 @@ void sim_rule_2(Cell environment[], Cell updt_environment[])
                 {
                     // faz o calcula para transferir as duas, transfere 1/4 para ambas e pula
                     flow_out = source_cell->fill_level / 2;
+                    if(flow_out < 0.005)
+                        continue;
+
                     flow_l = flow_out / 2;
                     flow_r = flow_out / 2;
 
                     updt_source_cell->fill_level -= flow_out;
                     updt_right_cell->fill_level += flow_r;
                     updt_left_cell->fill_level += flow_l;
+
+                    updt_source_cell->flow_direction = 4;
+                    //updt_right_cell->flow_direction = -1;
+                    //updt_left_cell->flow_direction = -1;
                     continue;
                 }
                 if(right_cell->type == WATER_TYPE)
                 {
                     // calcula para o lado direito apenas
                     flow_out = source_cell->fill_level / 3;
+                    if(flow_out < 0.005)
+                        continue;
+
                     flow_r = flow_out;
                     updt_source_cell->fill_level -= flow_out;
                     updt_right_cell->fill_level += flow_r;
+
+                    updt_source_cell->flow_direction = 2;
+                    //updt_right_cell->flow_direction = -1;
                 }
                 else if(left_cell->type == WATER_TYPE)
                 {
                     // calcula para o lado esquerdo apenas
                     flow_out = source_cell->fill_level / 3;
+                    if(flow_out < 0.005)
+                        continue;
+
                     flow_l = flow_out;
                     updt_source_cell->fill_level -= flow_out;
                     updt_left_cell->fill_level += flow_l;
+
+                    updt_source_cell->flow_direction = 3;
+                    //updt_left_cell->flow_direction = -1;
                 }
+            }
+        }
+    }
+    // atualiza o array original com as mudanças
+    for(int i=0; i<ROWS*COLUMNS; i++)
+    {
+        environment[i] = updt_environment[i];
+    }
+}
+
+void sim_rule_3(Cell environment[], Cell updt_environment[])
+{   /*
+    Regra #3: 
+        - Se não pode mais executar nem a regra 1, nem a regra 2 e
+        a célula está pressurizada (fill_value > 1). 
+        Então passa todo volume extra para célula acima, ficando apenas com
+        volume de 1: flow = fill_value - 1
+    */
+    Cell *source_cell, *cell_above;
+    Cell *updt_source_cell, *updt_cell_above;
+    float flow = 0;
+    for(int i=0; i<ROWS; i++)
+    {
+        for(int j=0; j<COLUMNS; j++)
+        {
+            source_cell = &environment[j + COLUMNS*i];
+            if(source_cell->type == SOLID_TYPE || i == 0)
+                continue;
+            
+            cell_above = &environment[j + COLUMNS*(i-1)];
+            if(cell_above->type == SOLID_TYPE || source_cell->fill_level < 0.0005)
+                continue;
+
+            updt_source_cell = &updt_environment[j + COLUMNS*i];
+            updt_cell_above = &updt_environment[j + COLUMNS*(i-1)];
+            if(source_cell->flow_direction == -1)
+            {
+                flow = source_cell->fill_level - 1;
+                if(flow < 0.005)
+                    continue;
+                updt_source_cell->fill_level -= flow;
+                updt_cell_above->fill_level += flow;
+                updt_source_cell->flow_direction = 0;
             }
         }
     }
@@ -305,10 +430,17 @@ void simulation_step(Cell environment[], Cell buffer_environment[])
         - se puder transferir para ambos os lados: transfere 1/4 para cada lado
         - se puder transferir para os dois lados: transfere 1/3 para o lado
 
+    Regra #3: 
+        - Se a célula está pressurizada (fill_value > 1), e 
+        não pode mais executar nem a regra 1, nem a regra 2. 
+        Então passa todo volume extra para célula acima, ficando apenas com
+        volume de 1: flow = fill_value - 1
+
     */
-   sim_rule_1(environment, buffer_environment);
-   // TODO: refinar regra 2, parece que estou perdendo volume de água
-   sim_rule_2(environment, buffer_environment);
+    sim_rule_1(environment, buffer_environment);
+    // TODO: refinar regra 2, parece que estou perdendo volume de água
+    sim_rule_2(environment, buffer_environment);
+    sim_rule_3(environment, buffer_environment);
 }
 
 int main()
@@ -358,10 +490,11 @@ int main()
         if(IsKeyPressed(KEY_D))
             del_mode = !del_mode;
 
+            
         //fazer os passos da simulação
         simulation_step(environment, buffer_environment);
-
         draw_environment(environment);
+
         draw_grid();
         DrawFPS(10, 10);
         
